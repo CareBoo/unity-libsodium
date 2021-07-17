7902086a68641ffddbe86dce12036b3c5de641f3
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-07-16 18:08:32 -0700

    fix win64

--- build_scripts/win64.sh
+++ build_scripts/win64.sh
@@ -2,5 +2,5 @@ wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.18-msvc.zip
 unzip libsodium-1.0.18-msvc.zip -d msvc
 
 mkdir -p output/builds
-basedir=msvc/x64/Debug/v140/dynamic
+basedir=msvc/libsodium/x64/Debug/v140/dynamic
 cp $basedir/libsodium.dll $basedir/libsodium.pdb output/builds
