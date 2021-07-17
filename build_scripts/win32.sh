cdb1cb523299ff666334a622c3bec097f06bdc55
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-07-16 17:55:44 -0700

    Fix win32 build

--- build_scripts/win32.sh
+++ build_scripts/win32.sh
@@ -2,5 +2,5 @@ wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.18-msvc.zip
 unzip libsodium-1.0.18-msvc.zip -d msvc
 
 mkdir -p output/builds
-basedir=msvc/Win32s/Debug/v140/dynamic
+basedir=msvc/Win32/Debug/v140/dynamic
 cp $basedir/libsodium.dll $basedir/libsodium.pdb output/builds
