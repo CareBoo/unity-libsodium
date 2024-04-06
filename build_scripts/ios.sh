2fc636f8585feac4ce96e20549c73146b276a985
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2024-04-05 21:50:57 -0700

    update build gh action to match the libsodium gh actions

--- build_scripts/ios.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd libsodium
-./dist-build/ios.sh
-
-cd -
-mkdir -p output/builds
-cp libsodium/libsodium-ios/lib/libsodium.a output/builds/libsodium.a
--- build_scripts/win64.sh
+++
@@ -1,6 +0,0 @@
-wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.18-msvc.zip
-unzip libsodium-1.0.18-msvc.zip -d msvc
-
-mkdir -p output/builds
-basedir=msvc/libsodium/x64/Debug/v140/dynamic
-cp $basedir/libsodium.dll $basedir/libsodium.pdb output/builds
